5784f5c9550646492828f41f1353b9984517d78c
Henrique Assis <154931368+Jose-Henrique-a@users.noreply.github.com>
2024-06-18 22:23:53 -0300

    test/refactor

--- src/uscs/Gato.java
+++ src/uscs/Gato.java
@@ -3,6 +3,7 @@ public class Gato extends Animal {
     public Gato(String nome, int idade){
         super(nome, idade);
     }
+    
     @Override
     public void emitirSom(){
         System.out.println("O gato " + nome + " de " +idade +" anos de idade, está miando.");
